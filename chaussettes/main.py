fffbd0d02f9334ad228fc38f3ce38c542208f610
Luc J. Bourhis <luc_j_bourhis@mac.com>
2017-07-18 22:40:28 +0900

    Functional GUI
    - implementation changed to a class
    - list hosts in ~/.ssh/config in menu
    - clicking an item establish a dynamic proxy to the associated host

--- chaussettes/main.py
+++ chaussettes/main.py
@@ -1,36 +1,71 @@
+from os import path
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
 gi.require_version('AppIndicator3', '0.1')
 from gi.repository import AppIndicator3 as appindicator
 
-APPINDICATOR_ID = 'fr.ljbo.chaussette'
+from chaussettes import ssh
 
-def main():
-  # Basic setup
-  indicator = appindicator.Indicator.new(
-    APPINDICATOR_ID,
-    'Chaussette',
-    appindicator.IndicatorCategory.SYSTEM_SERVICES)
-  indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+class Chaussettes:
 
-  # Build menu
-  menu = gtk.Menu()
+  APPINDICATOR_ID = 'fr.ljbo.chaussette'
 
-  # Quit
-  item_quit = gtk.MenuItem('Quit')
-  item_quit.connect('activate', quit)
-  menu.append(item_quit)
+  def __init__(self):
+    # Basic setup
+    self.indicator = appindicator.Indicator.new(
+      self.APPINDICATOR_ID,
+      'n/a',
+      appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 
-  # Finish menu setup
-  menu.show_all()
-  indicator.set_menu(menu)
+    # Build menu
+    self.menu = gtk.Menu()
 
-  # Event loop
-  gtk.main()
+    # Populate menu with ssh hosts
+    config = ssh.Config(path.expanduser('~/.ssh/config'))
+    self.selectable = []
+    self.hosts = config.hosts
+    for h in self.hosts:
+      item = gtk.CheckMenuItem(
+        '{}\n\t{}'.format(h.host, h.hostname) if h.hostname else
+        h.host)
+      item.connect('toggled', self.select, h)
+      self.menu.append(item)
+      self.selectable.append(item)
 
-def quit(source):
-  gtk.main_quit()
+    # Quit
+    self.menu.append(gtk.SeparatorMenuItem())
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', self.quit)
+    self.menu.append(item_quit)
+
+    # Finish menu setup
+    self.menu.show_all()
+    self.indicator.set_menu(self.menu)
+
+  def main(self):
+    gtk.main()
+
+  def select(self, item, host):
+    if not item.get_active():
+      # item has been disabled
+      host.disconnect()
+    else:
+      # item has been enabled
+      host.connect()
+      for item1 in self.selectable:
+        if item1 is not item:
+          item1.set_active(False)
+
+  def quit(self, source):
+    for h in self.hosts:
+      h.disconnect()
+    gtk.main_quit()
 
 if __name__ == "__main__":
-  main()
+  import logging
+  ssh.module_logger.setLevel(logging.INFO)
+  ssh.module_logger.addHandler(logging.StreamHandler())
+  Chaussettes().main()
